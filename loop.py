66fc15b4db8eef3cbd5dbc3f406dc597ab39f1e0
snehadharel26 <snehadharel26@gmail.com>
2024-11-17 09:46:42 +0545

    loop

---
+++ loop.py
@@ -0,0 +1,28 @@
+#WAP to print your name 5 times.
+# for i in range(0,5):
+#     print("SNEHA")
+
+#WAP to ask input from user and print it 5 times. 
+# name=input("enter your name:")
+# for i in range(5):
+#     print(name)
+
+#WAP to print even number from 10-40.
+# for num in range(10,41):
+#     if num % 2 == 0:
+#         print(num)
+
+#Another processs
+# for i in range(10,40,2):
+#     print(i)
+
+#WAP to print from 50-10 odd numbers.
+# for num in range(49, 9, -2):
+#     print(num, end=' ')
+
+#Another process
+#end=' ' will print in horizontal line.
+# for number in range(50, 9, -1): 
+#     if number % 2 != 0:  
+#         print(number, end=' ')
+
