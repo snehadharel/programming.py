79849bc760e468ff6a2376edfbe0dc3b682172c5
snehadharel26 <snehadharel26@gmail.com>
2024-12-02 09:43:15 +0545

    class synatx

---
+++ classsA.py
@@ -0,0 +1,42 @@
+#Syntax
+#class classname:
+#      prgram part:
+# class A35:
+#     x=10
+#     y=20
+#     name="abc"
+
+# p1=A35
+# #creating objects
+# print("The value of x is",p1.x)
+# print("The value of y is",p1.y)
+# print("My name is",p1.name)
+
+#WAP to print personal details using "class syntax".
+# class PersonalDetails:
+#     name="sneha"
+#     age=19
+#     gender="Female"
+#     gmail="sneha@gmail.com"
+#     number="123456789"
+    
+# p1=PersonalDetails
+# print("My name is:", p1.name)
+# print("My age is:", p1.age)
+# print("Gender is:", p1.gender)
+# print(" Email is :", p1.gmail)
+# print("Phone  is :", p1.number)
+
+#Create a class named Person using init function to assign the values for name and age,gender, and gmail.
+class person:
+    def __init__(self,name,age,gender,gmail):
+        self.name=name
+        self.age=age
+        self.gender=gender
+        self.gmail=gmail
+p1=person("Sneha",19,"Female","sneha@gmail.com")
+
+print("My name is ",p1.name,".")
+print("My age is",p1.age,"years old.")
+print("gender",p1.gender,".")
+print("My gmail is",p1.gmail,".")
