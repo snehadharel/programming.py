3b820e167dba583263a5b0b4e07bc2ee1ef222f7
snehadharel26 <snehadharel26@gmail.com>
2025-01-02 08:53:43 +0545

    single link list

---
+++ singlylld.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("Null")
+
+my_list = LinkedList()
+my_list.append(1)  
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+my_list.append(6)
+my_list.append(7)
+my_list.print_list()
